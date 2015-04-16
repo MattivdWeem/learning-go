e0459341cb90d50378bcfab09026756ddcb18847
Matti van de Weem <mattiweem@hotmail.com>
2015-04-16 13:06:12 +0200

    More simple funcions :)

--- simpleMath/simpleMath.go
+++ simpleMath/simpleMath.go
@@ -6,11 +6,21 @@ import ("fmt"; "math"; "math/rand"; "time")
 func main(){
 	rand.Seed( time.Now().UTC().UnixNano())
 
-	fmt.Println(math.Pi)
 	fmt.Println(randInt(0,400))
+	fmt.Println(plus(20,40))
+	fmt.Println(minus(40,20))
+	fmt.Println(math.Pi)
 }
 
 
 func randInt(min int, max int) int {
     return min + rand.Intn(max-min)
+}
+
+func plus(x int, y int) int {
+	return x + y
+}
+
+func minus(x int, y int) int {
+	return x - y
 }
