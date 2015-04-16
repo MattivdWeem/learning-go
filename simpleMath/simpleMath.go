bea9128f45e21a4704937b14264403c072302678
Matti van de Weem <mattiweem@hotmail.com>
2015-04-16 12:57:50 +0200

    Generate random numbers

--- simpleMath/simpleMath.go
+++ simpleMath/simpleMath.go
@@ -1,8 +1,16 @@
 package main
 
 
-import ("fmt"; "math")
+import ("fmt"; "math"; "math/rand"; "time")
 
 func main(){
-	fmt.Println(math.Pi);
+	rand.Seed( time.Now().UTC().UnixNano())
+
+	fmt.Println(math.Pi)
+	fmt.Println(randInt(0,400))
+}
+
+
+func randInt(min int, max int) int {
+    return min + rand.Intn(max-min)
 }
